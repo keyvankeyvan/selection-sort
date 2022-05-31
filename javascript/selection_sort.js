b4e7883e97f8ce153b247a7498b810d274e3b61a
Keyvan Alonso <keyvanalonso@gmail.com>
2022-05-31 12:00:07 +0200

    completed lab

--- javascript/selection_sort.js
+++ javascript/selection_sort.js
@@ -1,5 +1,19 @@
 function selectionSort(arr) {
-  // type your code here
+  const newArr = []
+  while(arr.length > 0) {
+    let min = Math.min(...arr)
+    let indx = arr.indexOf(min)
+    // for(number of arr){
+    //   if(number < min){
+    //     min = number
+    //     indx = arr.indexOf(number)
+    //   }
+    // }
+    newArr.push(min)
+    arr.splice(indx,1)
+  }
+
+  return newArr
 }
 
 if (require.main === module) {
@@ -10,14 +24,29 @@ if (require.main === module) {
   console.log("");
 
   // BENCHMARK HERE, and print the average runtime
-  const longInput = [];
-
-  for (let i = 0; i < 100; ++i) {
-    longInput.push(Math.random());
+  let start = Date.now()
+  
+  for(let i = 0; i < 1000; i++){
+    let longInput = longMaker()
+    selectionSort(longInput)
   }
+  let avgLongTime = (Date.now() - start)
+  
+  console.log(avgLongTime)
 }
 
 module.exports = selectionSort;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+
+function longMaker()
+{
+  let longInput = [];
+
+  for (let i = 0; i < 100; ++i) {
+    longInput.push(Math.random());
+  }
+  return longInput
+}
